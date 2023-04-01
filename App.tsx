7a323617327bf60a6230ee56cf65c455b7e769ea
Paul Buscano <pbuscano@deloitte.com>
2023-04-01 09:58:39 +0800

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,8 +1,62 @@
-import { StyleSheet, Text, View } from 'react-native';
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
+  const [location, setLocation] = useState();
+  const [address, setAddress] = useState();
+
+  Location.setGoogleApiKey('AIzaSyDr0K3wrFwCwJzLCZM172D44pTywulZmmw');
+
+  useEffect(() => {
+    const getPermissions = async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+
+      if (status !== 'granted') {
+        console.log('Please grant permission');
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+      console.log('Location:');
+      console.log(currentLocation);
+    };
+
+    getPermissions();
+  }, []);
+
+  const geocode = async () => {
+    const geocodedLocation = await Location.geocodeAsync(address);
+    console.log(geocodedLocation);
+  };
+
+  const reverseGeocode = async () => {
+    const reverseGeocodedAddress = await Location.reverseGeocodeAsync(
+      {
+        longitude: location.coords.longitude,
+        latitude: location.coords.latitude,
+      },
+      {
+        useGoogleMaps: true,
+      }
+    );
+
+    console.log(reverseGeocodedAddress);
+  };
+
   return (
     <View style={styles.container}>
+      <TextInput
+        placeholder="Address"
+        value={address}
+        onChangeText={setAddress}
+      />
+      <Button title="Geocode Address" onPress={geocode} />
+      <Button
+        title="Reverse Geocode current location"
+        onPress={reverseGeocode}
+      />
       <Text>Open up App.tsx to start working on your app!</Text>
     </View>
   );
